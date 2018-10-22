e61b9dfeff551bb7fbab593f1662f60669b21255
Quinton Bolt <qbolt@cooksys.com>
2018-10-22 17:01:41 -0500

    Added schema.sql

---
+++ schema.sql
@@ -0,0 +1,32 @@
+-- SCHEMA: smartshare
+
+-- DROP SCHEMA smartshare ;
+
+CREATE SCHEMA smartshare
+    AUTHORIZATION postgres;
+
+-- Table: smartshare.files
+
+-- DROP TABLE smartshare.files;
+
+CREATE TABLE smartshare.files
+(
+    file_name text COLLATE pg_catalog."default" NOT NULL,
+    id serial,
+    file bytea,
+    time_created timestamp without time zone NOT NULL,
+    expiry_time timestamp without time zone NOT NULL,
+    max_downloads integer NOT NULL,
+    total_downloads integer NOT NULL,
+    password text COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT files_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+
+TABLESPACE pg_default;
+GRANT USAGE ON SEQUENCE smartshare.files_id_seq TO postgres;
+
+ALTER TABLE smartshare.files
+    OWNER to postgres;
